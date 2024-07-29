52d403729d29137ce516b65f7e2f07267e1479f3
GameCharmer <1208565+GameCharmer@users.noreply.github.com>
2024-07-29 08:41:06 -0400

    Initial Commit, EnumToArray Trait

---
+++ README.md
@@ -0,0 +1,6 @@
+# GCWorld Enumerations
+
+Eventual home to standardized enums for GCWorld products
+
+### Version
+0.0.0
---
+++ src/Traits/EnumToArrayTrait.php
@@ -0,0 +1,45 @@
+<?php
+namespace GCWorld\Enums\Traits\EnumTraits;
+
+/**
+ * EnumToArrayTrait trait.
+ */
+trait EnumToArrayTrait
+{
+    /**
+     * @return array
+     */
+    public static function textArray(): array
+    {
+        $tmp = [];
+        foreach (self::cases() as $k => $v) {
+            $tmp[$k] = $v->text();
+        }
+
+        return $tmp;
+    }
+
+    /**
+     * @return array
+     */
+    public static function nameArray(): array
+    {
+        return \array_column(self::cases(), 'name');
+    }
+
+    /**
+     * @return array
+     */
+    public static function valuesArray(): array
+    {
+        return \array_column(self::cases(), 'value');
+    }
+
+    /**
+     * @return array
+     */
+    public static function array(): array
+    {
+        return \array_combine(self::valuesArray(), self::textArray());
+    }
+}
